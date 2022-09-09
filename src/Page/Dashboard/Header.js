e66f4ff1bb18647ee6022ce82cd423d514c6c438
ivipularora <112922718+ivipularora@users.noreply.github.com>
2022-09-09 11:23:52 +0530

    Update Header.js
    ASAP ASAP

--- src/Page/Dashboard/Header.js
+++ src/Page/Dashboard/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-function Header({ setIsAdding }) {
+function Header({ setIsAdding, attendance }) {
+    console.log(attendance)
+    var date = new Date();
+	var current_date = date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
     return (
         <header>
+            <div style={{display:'flex', justifyContent:'space-between'}}>
             <h1>Attendance Management CRUD</h1>
-            <div style={{ marginTop: '30px', marginBottom: '18px'}}>
+            <h4 style={{textAlign: 'right'}}>Date: {current_date}</h4>
+            </div>
+            <div style={{ marginTop: '30px', marginBottom: '18px' , display:'flex', justifyContent:'space-between'}}>
                 
                 <button onClick={() => setIsAdding(true)} className='round-button'>Add Employee</button>
-               
+                <h4 style={{textAlign: 'right'}}>present : {attendance}</h4>
             </div>
         </header>
     )
